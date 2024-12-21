b406576f535e77983adf294d514b5722b7f104a0
Yuriko Schumacher <yuriko.schumacher@gmail.com>
2024-12-21 14:21:57 -0600

    update

--- src/Components/Work.svelte
+++ src/Components/Work.svelte
@@ -11,7 +11,6 @@
   <div class="work__intro">
     <h1>My work:</h1>
     <p>This selection showcases some of my best work, ranging from my projects at Northeastern University to my recent contributions at the Texas Tribune. Stars <i class="fa-solid fa-star"></i> indicate the selection of work that I'm the most proud of. Other ones are sorted {widont("by date.")}</p>
-    <p>Each card represents a project and contains the publication date, the organization I worked with, a brief description, the skills applied, my roles, and the types of visualizations used in the project.</p>
   </div>
 
   <div class="work-cards">
